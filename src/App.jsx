cb42a6257b35b3d3f5fa35663ca0e2d09b167afb
Eugene <zh_kononenko3@mail.ru>
2022-10-30 21:42:24 +0300

    Add comments

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,11 @@ export function App() {
 
 	const [dice, setDice] = useState(getRandomDice());
 
+	// array of <Die> components
 	const diceArray = dice.map(die =>
 		<Die key={die.id} num={die.num} isHeld={die.isHeld} hold={() => holdDice(die.id)} />)
 
+	// return array of objects(dice) with random id and number
 	function getRandomDice() {
 		return Array.from({ length: 10 }, () => {
 			return {
@@ -20,9 +22,12 @@ export function App() {
 		});
 	}
 
+	// randomize dice numbers
 	function roll() {
 		setDice(getRandomDice());
 	}
+
+	// flip isHeld prop of Die component
 	function holdDice(id) {
 		setDice(prev => prev.map((die) => {
 			return die.id === id ?
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Die = ({num, isHeld, hold}) => {
     return (
+        //if isHeld === true then add classname "held" 
         <div className={isHeld?"die held":"die"} onClick={hold}>
             <h1>{num}</h1>
         </div>
