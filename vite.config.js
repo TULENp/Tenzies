04907b568d5e6e30b798ce62059342f13a94c08d
Eugene <zh_kononenko3@mail.ru>
2022-11-01 21:59:17 +0300

    Add sourcemap to vite.config And add !important to button color

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  devtool: 'inline-source-map',
+  build: {
+    sourcemap: 'inline',
+  },
 })
