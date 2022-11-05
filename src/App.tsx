65ee2364725fb0dab147d045720d42eccf8f36ff
Eugene <zh_kononenko3@mail.ru>
2022-11-06 00:42:06 +0300

    Add changeDifficulty() and roll counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef, Fragment } from 'react'
 import Confetti from 'react-confetti' // for congrats confetti
 import './App.css'
 import Die from './components/Die'
 import { nanoid } from 'nanoid'; // to generate random ids
 
-//todo Add rolls counter and timer
+
+//todo maybe add timer
 //todo Save best time to localStorage
+//todo add difficulty mods (5, 10, 15 dice eg / ?timer or limited rolls?)
 
 export function App(): JSX.Element {
 
@@ -14,9 +16,17 @@ export function App(): JSX.Element {
 		num: number,
 		isHeld: boolean
 	}
+	const diffNames = { insane: "insane :)", normal: "normal", hard: "hard" };
+	const diffs = { normal: 10, hard: 15, insane: 5 };
 
-	const [dice, setDice] = useState<TDice[]>(getRandomDice());
 	const [isWon, setIsWon] = useState<boolean>(false);
+	const [counter, setCounter] = useState<number>(0)
+	const [winText, setWinText] = useState<string>("")
+	const [diffName, setDiffName] = useState<string>(diffNames.normal)
+
+	let difficulty = diffs.normal;
+	const [dice, setDice] = useState<TDice[]>(getRandomDice(difficulty));
+
 	const shake = useRef<HTMLDivElement>(HTMLDivElement.prototype);
 
 	// array of <Die/> components 
@@ -30,12 +40,13 @@ export function App(): JSX.Element {
 
 		if (allHeld && allSameNum) {
 			setIsWon(true);
+			setWinText((counter > 0) ? "You Won!" : "Lucky one!")
 		}
 	}, [dice]);
 
 	// return array of objects(dice) with random id and number
-	function getRandomDice(): TDice[] {
-		return Array.from({ length: 10 }, () => {
+	function getRandomDice(diff: number): TDice[] {
+		return Array.from({ length: difficulty }, () => {
 			return {
 				id: nanoid(),
 				num: Math.ceil(Math.random() * 6),
@@ -54,13 +65,37 @@ export function App(): JSX.Element {
 			}))
 		} else { // if isWon is true reset game
 			setIsWon(false);
-			setDice(getRandomDice());
+			setDice(getRandomDice(difficulty));
+			setCounter(-1);
 		}
-		
+		setCounter(prev => prev + 1);
+
 		shake.current.className = 'dice shake';
 		setTimeout(() => {
 			shake.current.className = 'dice';
 		}, 200);
+	}
+
+	// change number of dice (5 | 10 | 15) and restart game
+	function changeDifficulty() {
+		if (!isWon) {
+			switch (diffName) {
+				case diffNames.insane:
+					difficulty = diffs.normal;
+					setDiffName(diffNames.normal);
+					break;
+				case diffNames.normal:
+					difficulty = diffs.hard;
+					setDiffName(diffNames.hard);
+					break;
+				case diffNames.hard:
+					difficulty = diffs.insane;
+					setDiffName(diffNames.insane);
+					break;
+			}
+			setDice(getRandomDice(difficulty));
+		}
+
 	}
 
 	// flip isHeld prop of Die component
@@ -79,18 +114,31 @@ export function App(): JSX.Element {
 				<div className="block">
 					<div className="info">
 						{isWon
-							? <h1>You Won!</h1>
-							: <>
+							? <Fragment>
+								<h1>{winText}</h1>
+								<h2><a href="https://github.com/TULENz" target="_blank">Check my GitHub for... something interesting</a></h2>
+							</Fragment>
+							: <Fragment>
 								<h1>Tenzies</h1>
 								<h2>Roll until all dice are the same.</h2>
 								<h2>Click each die to freeze it at its current value between rolls.</h2>
-							</>
+							</Fragment>
 						}
 					</div>
 					<div className='dice' ref={shake}>
 						{diceArray}
 					</div>
-					<button className='rollButton' onClick={roll}>{isWon ? "New Game" : "Roll"}</button>
+					<div className='tools'>
+						<div className='counter'>
+							<h2><b>Roll counter:</b></h2>
+							<h2>{counter}</h2>
+						</div>
+						<button className='rollButton' onClick={roll}>{isWon ? "New Game" : "Roll"}</button>
+						<div className='difficulty' onClick={changeDifficulty}>
+							<h2><b>Difficulty:</b></h2>
+							<h2>{diffName}</h2>
+						</div>
+					</div>
 				</div>
 			</section>
 			<p>© Created by <a href="https://github.com/TULENz" target="_blank">Eugene Kononenko</a></p>
